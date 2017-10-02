b63a4b12089fbe5e843fa1a31b9d0eb314ba0651
shadid12 <shadidhaque2012@gmail.com>
2017-10-02 10:30:10 -0400

    added working access

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-    'clientID': '273145355531-nenoe2jhntg9mc195n117o7iq8g7ane7.apps.googleusercontent.com',
-    'clientSecret': 'FurKP_ou-GBdYxVG2Lv3isAh'
+    'googleClientID': '273145355531-nenoe2jhntg9mc195n117o7iq8g7ane7.apps.googleusercontent.com',
+    'googleClientSecret': 'FurKP_ou-GBdYxVG2Lv3isAh'
 }
