e2632ebfb7150bbefb34c1f1e48dc0b96ab1e9f9
shadid12 <shadidhaque2012@gmail.com>
2017-10-03 00:41:34 -0400

    awesome things happening now

--- server.js
+++ server.js
@@ -1,36 +1,10 @@
 const express = require('express');
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const keys = require("./config/keys");
+const mongoose = require('mongoose');
+require('./services/passport');
+
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('ohh hello world');
-});
-
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: keys.googleClientID,
-      clientSecret: keys.googleClientSecret,
-      callbackURL: '/auth/google/callback'
-    },
-    (accessToken, refreshToken, profile, done) => {
-      console.log('access token', accessToken);
-      console.log('refresh token', refreshToken);
-      console.log('profile', profile);
-    }
-  )
-);
-
-app.get(
-  '/auth/google',
-  passport.authenticate('google', {
-    scope: ['profile', 'email']
-  })
-);
-
-app.get('/auth/google/callback', passport.authenticate('google'));
+require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT);
