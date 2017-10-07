0aa653c04b571d29083c1044e2cba482d0ffba3e
shadid12 <shadidhaque2012@gmail.com>
2017-10-07 00:57:18 -0400

    auth with thunk reducers

---
+++ client/src/components/Header.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Header</h2>
+        <button>Sign in with Google</button>
+      </div>
+    );
+  }
+}
+
+export default Header;
